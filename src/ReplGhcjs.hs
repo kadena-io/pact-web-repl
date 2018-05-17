254b9f830f8e22f438c40eb21020df6899e97447
Doug Beardsley <mightybyte@gmail.com>
2018-05-17 19:53:15 -0400

    Simplify REPL code and show all output

--- src/ReplGhcjs.hs
+++ src/ReplGhcjs.hs
@@ -46,7 +46,11 @@ app = do
       let changeExample = getDemo <$> updated (value d)
       code <- elAttr "div" ("id" =: "column1") $ codeWidget hello changeExample
       elAttr "div" ("id" =: "separator" <> "class" =: "separator") blank
-      elAttr "div" ("id" =: "column2") $ replWidget $ tag (current code) le
+      elAttr "div" ("id" =: "column2") $ do
+        elAttr "div" ("id" =: "code") $ do
+          mapM_ snippetWidget staticReplHeader
+          widgetHold blank (replWidget <$> tag (current code) le)
+          return ()
 
 codeWidget :: MonadWidget t m => Text -> Event t Text -> m (Dynamic t Text)
 codeWidget iv sv = do
@@ -63,8 +67,8 @@ data DisplayedSnippet
   | OutputSnippet Text
   deriving (Eq,Ord,Show,Read)
 
-dummyData :: Seq DisplayedSnippet
-dummyData = S.fromList
+staticReplHeader :: Seq DisplayedSnippet
+staticReplHeader = S.fromList
       [ OutputSnippet ";; Welcome to the Pact interactive repl"
       , OutputSnippet ";; Enter pact commands here"
       ]
@@ -73,54 +77,49 @@ snippetWidget :: MonadWidget t m => DisplayedSnippet -> m ()
 snippetWidget (InputSnippet t) = el "pre" $ text t
 snippetWidget (OutputSnippet t) = el "pre" $ text t
 
+------------------------------------------------------------------------------
 replWidget
     :: MonadWidget t m
-    => Event t Text
+    => Text
     -> m ()
-replWidget newCode = do
-    initState <- liftIO $ initReplState Interactive
-    let runInput input snippets =
-          snippets <> S.singleton (InputSnippet input)
+replWidget initialCode = mdo
+    initState <- liftIO $ initReplState StringEval
+    stateAndOut0 <- runReplStep0 (initState, mempty) initialCode
+    stateAndOut <- holdDyn stateAndOut0 evalResult
 
-    elAttr "div" ("id" =: "code") $ mdo
-      let start = (Left "init", initState)
-      replState <- holdDyn start $ leftmost
-        [ evalResult
-        , start <$ newCode
-        ]
-      snippets <- holdUniqDyn =<< foldDyn ($) dummyData (leftmost
-        [ runInput <$> newInput
-        , (\r ss -> ss <> S.singleton (OutputSnippet (showResult $ fst r))) <$> evalResult
-        , const dummyData <$ newCode
-        ])
-      _ <- dyn $ mapM_ snippetWidget <$> snippets
-      (ti, goEvent) <- divClass "repl-input-controls" $ mdo
-        ti <- textArea $ def
-          & attributes .~ constDyn mempty -- ("class" =: "repl-input")
-          & setValue .~ (mempty <$ buttonClicked)
-        (b,_) <- el' "button" $ text "Evaluate"
-        let buttonClicked = domEvent Click b
-        return (ti, buttonClicked)
-      let newInput = tag (current $ value ti) goEvent
-      evalResult <- performEvent $ leftmost
-        [ attachWith runExpr (current replState) newInput
-        , attachPromptlyDynWith runExpr replState newCode
-        ]
-      return ()
+    _ <- dyn (mapM_ snippetWidget . snd <$> stateAndOut)
+    newInput <- replInput
+    evalResult <- performEvent $
+      attachWith runReplStep (current stateAndOut) newInput
+    return ()
 
-runExpr
+replInput :: MonadWidget t m => m (Event t Text)
+replInput = do
+    divClass "repl-input-controls" $ mdo
+      ti <- textArea $ def
+        & attributes .~ constDyn mempty -- ("class" =: "repl-input")
+        & setValue .~ (mempty <$ buttonClicked)
+      (b,_) <- el' "button" $ text "Evaluate"
+      let buttonClicked = domEvent Click b
+      return $ tag (current $ value ti) buttonClicked
+
+runReplStep0
     :: MonadIO m
-    => (Either String (Term Name), ReplState)
+    => (ReplState, Seq DisplayedSnippet)
     -> Text
-    -> m (Either String (Term Name), ReplState)
-runExpr (_,s) e = liftIO $ runStateT (evalRepl' $ T.unpack e) s
+    -> m (ReplState, Seq DisplayedSnippet)
+runReplStep0 (s1,snippets1) e = do
+    (eterm,s2) <- liftIO $ runStateT (evalRepl' $ T.unpack e) s1
+    return (s2, snippets1 <> S.singleton (OutputSnippet $ T.pack $ _rOut s2))
 
-myEvalString :: Text -> IO Text
-myEvalString cmd = do
-  (er,_) <- initReplState StringEval >>= runStateT (evalRepl' $ T.unpack cmd)
-  return $ case er of
-    (Right v) -> T.pack $ show v
-    (Left e) -> "Error: " <> T.pack e
+runReplStep
+    :: MonadIO m
+    => (ReplState, Seq DisplayedSnippet)
+    -> Text
+    -> m (ReplState, Seq DisplayedSnippet)
+runReplStep (s1,snippets1) e = do
+    (eterm,s2) <- liftIO $ runStateT (evalRepl' $ T.unpack e) s1
+    return (s2, snippets1 <> S.fromList [InputSnippet e, OutputSnippet $ showResult eterm])
 
 showResult :: Show a => Either String a -> Text
 showResult (Right v) = T.pack $ show v
