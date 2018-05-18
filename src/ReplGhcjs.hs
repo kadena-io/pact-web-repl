1dbd15553aa962d2c8085bd69dc4a073cfe01687
Doug Beardsley <mightybyte@gmail.com>
2018-05-18 16:00:44 -0400

    Set focus, scroll position for convenience

--- src/ReplGhcjs.hs
+++ src/ReplGhcjs.hs
@@ -12,6 +12,7 @@
 module Main where
 
 ------------------------------------------------------------------------------
+import           Control.Lens
 import           Control.Monad.State.Strict
 import           Data.Map (Map)
 import qualified Data.Map as M
@@ -20,6 +21,7 @@ import           Data.Sequence (Seq)
 import qualified Data.Sequence as S
 import           Data.Text (Text)
 import qualified Data.Text as T
+import           Language.Javascript.JSaddle
 import           Reflex
 import           Reflex.Dom hiding (Element, fromJSString)
 import           Reflex.Dom.ACE
@@ -46,11 +48,18 @@ app = do
       let changeExample = getDemo <$> updated (value d)
       code <- elAttr "div" ("id" =: "column1") $ codeWidget hello changeExample
       elAttr "div" ("id" =: "separator" <> "class" =: "separator") blank
-      elAttr "div" ("id" =: "column2") $ do
+      (e,newExpr) <- elAttr' "div" ("id" =: "column2") $ do
         elAttr "div" ("id" =: "code") $ do
           mapM_ snippetWidget staticReplHeader
-          widgetHold blank (replWidget <$> tag (current code) le)
-          return ()
+          widgetHold (replWidget hello) (replWidget <$> tag (current code) le)
+      timeToScroll <- delay 0.1 $ switch $ current newExpr
+      _ <- performEvent (scrollToBottom (_element_raw e) <$ timeToScroll)
+      return ()
+
+scrollToBottom :: (PToJSVal t, MonadIO m) => t -> m ()
+scrollToBottom e = liftJSM $ do
+    let pElem = pToJSVal e
+    (pElem <# ("scrollTop" :: String)) (pElem ^. js ("scrollHeight" :: String))
 
 codeWidget :: MonadWidget t m => Text -> Event t Text -> m (Dynamic t Text)
 codeWidget iv sv = do
@@ -81,7 +90,7 @@ snippetWidget (OutputSnippet t) = el "pre" $ text t
 replWidget
     :: MonadWidget t m
     => Text
-    -> m ()
+    -> m (Event t Text)
 replWidget initialCode = mdo
     initState <- liftIO $ initReplState StringEval
     stateAndOut0 <- runReplStep0 (initState, mempty) initialCode
@@ -91,17 +100,17 @@ replWidget initialCode = mdo
     newInput <- replInput
     evalResult <- performEvent $
       attachWith runReplStep (current stateAndOut) newInput
-    return ()
+    return newInput
 
 replInput :: MonadWidget t m => m (Event t Text)
 replInput = do
     divClass "repl-input-controls" $ mdo
-      ti <- textArea $ def
-        & attributes .~ constDyn mempty -- ("class" =: "repl-input")
+      ta <- textArea $ def
         & setValue .~ (mempty <$ buttonClicked)
       (b,_) <- el' "button" $ text "Evaluate"
       let buttonClicked = domEvent Click b
-      return $ tag (current $ value ti) buttonClicked
+      _ <- performEvent (liftJSM (pToJSVal (_textArea_element ta) ^. js0 ("focus" :: String)) <$ buttonClicked)
+      return $ tag (current $ value ta) buttonClicked
 
 runReplStep0
     :: MonadIO m
@@ -109,7 +118,7 @@ runReplStep0
     -> Text
     -> m (ReplState, Seq DisplayedSnippet)
 runReplStep0 (s1,snippets1) e = do
-    (eterm,s2) <- liftIO $ runStateT (evalRepl' $ T.unpack e) s1
+    (_,s2) <- liftIO $ runStateT (evalRepl' $ T.unpack e) s1
     return (s2, snippets1 <> S.singleton (OutputSnippet $ T.pack $ _rOut s2))
 
 runReplStep
@@ -133,15 +142,28 @@ controlBar = do
     elAttr "div" ("id" =: "options") $ do
       elAttr "div" ("style" =: "display: block;") $ do
         o <- elAttr "ul" ("class" =: "view_mode") $ do
-          d <- elAttr "li" ("class" =: "code-example-dropdown") $ do
+          d <- elAttr "li" ("class" =: "code_examples_fieldset") $ do
             elAttr "fieldset" ("class" =: "code_examples_fieldset") $ do
-              elAttr "li" ("id" =: "code_examples_fieldset") $ do
                 dropdown 0 (constDyn $ fmap fst demos) def
           load <- elAttr "li" ("style" =: "padding-left: 10px;" <> "class" =: "tooltip") $ do
             (e,_) <- elAttr' "label" ("id" =: "compile_now") $ text "Load"
             return $ domEvent Click e
-            --elClass "span" "tooltiptext" $ text "Click or Shift-Enter"
+          elAttr "li" ("style" =: "padding-left: 10px;" <> "id" =: "pactVersion") $
+            elAttr "a" ("target" =: "_blank" <> "href" =: "https://github.com/kadena-io/pact") $ do
+              is <- liftIO $ initReplState StringEval
+              Right (TLiteral (LString ver) _) <- liftIO $ evalStateT (evalRepl' "(pact-version)") is
+              text $ "Pact Version " <> ver
           return (ControlOut d load)
         elAttr "ul" ("class" =: "view_mode" <> "style" =: "float: right") $ do
-          elAttr "img" ("src" =: "img/kadena-logo84x20.png" <> "class" =: "logo-image") blank
+          el "li" $
+            elAttr "label" ("style" =: "padding-left:0;") $
+              elAttr "a" ("target" =: "_blank" <>
+                          "style" =: "color:black;text-decoration:none;" <>
+                          "href" =: "http://pact-language.readthedocs.io"
+                          ) $ do
+                elAttr "i" ("class" =: "fa fa-book" <> "aria-hidden" =: "true") blank
+                elAttr "span" ("id" =: "hideIfTiny") $ text "Docs"
+          el "li" $
+            elAttr "a" ("target" =: "_blank" <> "href" =: "http://kadena.io") $
+              elAttr "img" ("src" =: "img/kadena-logo84x20.png" <> "class" =: "logo-image") blank
         return o
